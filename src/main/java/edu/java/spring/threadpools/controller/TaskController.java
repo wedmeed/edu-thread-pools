d1b84706d492b9887acd4b9b78a88ebe152d360e
asokolov <wedmeed@mail.ru>
2020-03-25 18:03:20 +0400

    config behavior for long answers

--- src/main/java/edu/java/spring/threadpools/controller/TaskController.java
+++ src/main/java/edu/java/spring/threadpools/controller/TaskController.java
@@ -4,10 +4,12 @@ import edu.java.spring.threadpools.model.FeasibilityResult;
 import edu.java.spring.threadpools.model.Task;
 import edu.java.spring.threadpools.service.FeasibilityService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,6 +25,7 @@ public class TaskController {
     }
 
     @PostMapping("/suit/{suit_name}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public void publishSalary(@PathVariable("suit_name") String suitName, @RequestBody List<Task> taskSuit) {
         service.publishTaskSuit(suitName, taskSuit);
     }
@@ -30,9 +33,15 @@ public class TaskController {
     @GetMapping("/feasibility/{suit_name}")
     public FeasibilityResult publishSalary(@PathVariable("suit_name") String suitName) {
         FeasibilityResult res = service.getSuitFeasibility(suitName);
+        if (res == null){
+            throw new ResultsArentReadyException();
+        }
         log.info("Return feasibility {}", res);
         return res;
     }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public class ResultsArentReadyException extends RuntimeException{}
 
 
 }
