2a8f816d7bae0d5c9bdba6aa0179550d39e8ed4d
asokolov <wedmeed@mail.ru>
2020-03-25 13:52:41 +0400

    initial

---
+++ src/main/java/edu/java/spring/threadpools/ThreadPoolsApplication.java
@@ -0,0 +1,13 @@
+package edu.java.spring.threadpools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadPoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadPoolsApplication.class, args);
+	}
+
+}
